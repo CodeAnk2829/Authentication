243dde97b67daf876a6b0fd1aa6724402213be7d
Ankit Kumar <104667338+CodeAnk2829@users.noreply.github.com>
2023-08-09 11:47:04 +0530

    Add level-4 encryption

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ import express from "express";
 import bodyParser from "body-parser";
 import ejs from "ejs";
 import mongoose from "mongoose";
-import md5 from "md5";
+import bcrypt from "bcrypt";
 
 
 const app = express();
 const PORT = 3000;
+const saltRounds = 10;
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
@@ -51,44 +52,67 @@ app.get("/register", async (req, res) => {
 
 // registration
 app.post("/register", async (req, res) => {
-    const userEmail = req.body.username;
-    // use hashing to hash password
-    const userPassword = md5(req.body.password);
-    
-    const isUserFound = await User.findOne({email: userEmail});
-    
-    // if user is not found
-    if(!isUserFound) {
-        // create a new user model
-        const user = new User ({
-            email: userEmail,
-            password: userPassword
-        });
-        
-        await user.save();
-        res.render("secrets");
-    } else {
-        console.log("User already exists.");
-        const message = "<p>*This email has already registered. Please login</p>"
-        res.render("register", {alreadyRegistered: message});
-    }
+    // encrypt the user's password using bcrypt hashing
+    // security level - 4
+    bcrypt.hash(req.body.password, saltRounds, async (err, hash) => {
+        if(err) {
+            console.log(err.message);
+        } else {
+            const userEmail = req.body.username;
+            // use hashing to hash password
+            const userPassword = hash;
+            
+            const isUserFound = await User.findOne({email: userEmail});
+            
+            // if user is not found
+            if(!isUserFound) {
+                // create a new user model
+                const user = new User ({
+                    email: userEmail,
+                    password: userPassword
+                });
+                
+                await user.save();
+                res.render("secrets");
+            } else {
+                console.log("User already exists.");
+                const alreadyRegistered = "<p>*This email has already been registered. Please login</p>"
+                res.render("login", {message: alreadyRegistered});
+            }
+        }
+    });
 });
 
 // login
 app.post("/login", async(req, res) => {
     const userEmail = req.body.username;
-    
-    const userPassword = md5(req.body.password);
+    const userPassword = req.body.password;
 
     const isUserFound = await User.findOne({email: userEmail});
 
-
-    if(isUserFound && (isUserFound.password === userPassword)) {
-        res.render("secrets");
+    // if user exists
+    if(isUserFound) {
+        // compare the password (which user has typed in) with the 
+        // password that has been stored corresponding to the same user
+        bcrypt.compare(userPassword, isUserFound.password, (err, result) => {
+            if(err) {
+                console.log(err);
+            } else {
+                if(result === true) {
+                    // if password matched
+                    res.render("secrets");
+                } else {
+                    // if password didn't match
+                    console.log("We don't have any account associated with this email.");
+                    const wrongCredential = "<p>*Invalid email or password.</p>"
+                    res.render("login",{onWrongCredential: wrongCredential});
+                }
+            }
+        });
     } else {
-        console.log("We don't have any account associated with this email.");
-        const wrongCredential = "<p>*Invalid email or password.</p>"
-        res.render("login",{onWrongCredential: wrongCredential});
+        // if user doesn't exist
+        const userNotRegisteredMessage = "<p>*Email doesn't exist. Please register</p>";
+        res.render("register", {message: userNotRegisteredMessage});
     }
 });
 
