acb2896b3de93fab6d90f45b6822083c9a72dc7c
Ankit Kumar <104667338+CodeAnk2829@users.noreply.github.com>
2023-08-10 22:13:53 +0530

    Add level-5 encryption

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ import express from "express";
 import bodyParser from "body-parser";
 import ejs from "ejs";
 import mongoose from "mongoose";
-import bcrypt from "bcrypt";
+import session from "express-session";
+import passport from "passport";
+import passportLocalMongoose from "passport-local-mongoose";
 
 
 const app = express();
@@ -13,6 +15,15 @@ const saltRounds = 10;
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+// It should be placed exactly here
+app.use(session({
+    secret: "our little secret.",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 mongoose.set("strictQuery", false);
 await mongoose.connect("mongodb://127.0.0.1:27017/userDB");
@@ -21,7 +32,7 @@ await mongoose.connect("mongodb://127.0.0.1:27017/userDB");
 
 // create a user schema
 const userSchema = new mongoose.Schema({
-    email: {
+    username: {
         type: String,
         required: true
     },
@@ -32,11 +43,14 @@ const userSchema = new mongoose.Schema({
     }
 });
 
-
+userSchema.plugin(passportLocalMongoose);
 
 // create model for user
 const User = mongoose.model("User", userSchema);
 
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", async (req, res) => {
     res.render("home");
@@ -50,70 +64,56 @@ app.get("/register", async (req, res) => {
     res.render("register");
 });
 
+app.get("/secrets", async (req, res) => {
+    if(req.isAuthenticated()) {
+        res.render("secrets");
+    } else {
+        res.redirect("/login");
+    }
+});
+
+// logout
+app.get("/logout", async (req, res) => {
+    req.logout(function(err) {
+        if (err) { return next(err); }
+        res.redirect('/');
+    });
+});
+
 // registration
 app.post("/register", async (req, res) => {
-    // encrypt the user's password using bcrypt hashing
-    // security level - 4
-    bcrypt.hash(req.body.password, saltRounds, async (err, hash) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    User.register({username: username}, password, function(err, user) {
         if(err) {
-            console.log(err.message);
+            console.log(err);
+            res.redirect("/register");
         } else {
-            const userEmail = req.body.username;
-            // use hashing to hash password
-            const userPassword = hash;
-            
-            const isUserFound = await User.findOne({email: userEmail});
-            
-            // if user is not found
-            if(!isUserFound) {
-                // create a new user model
-                const user = new User ({
-                    email: userEmail,
-                    password: userPassword
-                });
-                
-                await user.save();
-                res.render("secrets");
-            } else {
-                console.log("User already exists.");
-                const alreadyRegistered = "<p>*This email has already been registered. Please login</p>"
-                res.render("login", {message: alreadyRegistered});
-            }
+            passport.authenticate("local")(req, res, function() {
+                res.redirect("/secrets");
+            });
         }
-    });
+    }); 
 });
 
 // login
 app.post("/login", async(req, res) => {
-    const userEmail = req.body.username;
-    const userPassword = req.body.password;
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
 
-    const isUserFound = await User.findOne({email: userEmail});
-
-    // if user exists
-    if(isUserFound) {
-        // compare the password (which user has typed in) with the 
-        // password that has been stored corresponding to the same user
-        bcrypt.compare(userPassword, isUserFound.password, (err, result) => {
-            if(err) {
-                console.log(err);
-            } else {
-                if(result === true) {
-                    // if password matched
-                    res.render("secrets");
-                } else {
-                    // if password didn't match
-                    console.log("We don't have any account associated with this email.");
-                    const wrongCredential = "<p>*Invalid email or password.</p>"
-                    res.render("login",{onWrongCredential: wrongCredential});
-                }
-            }
-        });
-    } else {
-        // if user doesn't exist
-        const userNotRegisteredMessage = "<p>*Email doesn't exist. Please register</p>";
-        res.render("register", {message: userNotRegisteredMessage});
-    }
+    req.login(user, (err) => {
+        if(err) {
+            console.log(err);
+            res.redirect("/login");
+        } else {
+            passport.authenticate("local")(req, res, function() {
+                res.redirect("/secrets");
+            });
+        }
+    })
+    
 });
 
 app.listen(PORT, () => {
